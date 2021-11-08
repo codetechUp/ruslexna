6de0dc38aa3ce370850f316261d9fc73cbe299f4
codetechup <papasa97@gmail.com>
2021-11-08 18:23:11 +0000

    integration de chatbot

--- src/Controller/PaiementController.php
+++ src/Controller/PaiementController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use DateTime;
+use Paydunya;
 use App\Entity\Subscription;
 use App\Repository\UserRepository;
 use App\Repository\PacksRepository;
@@ -17,6 +18,22 @@ class PaiementController extends AbstractController
      */
     public function index($idpack,$iduser,PacksRepository $repac,UserRepository $repuser)
     {
+        Setup::setMasterKey("sO4bJTB1-VGno-uwPH-HRMT-6C5H4kNFu8Qn");
+Setup::setPrivateKey("live_private_542NI1K1OydZQRPB1mE2uWFgraY");
+Setup::setToken("9tIQowOQQxlNEhQaVIzl");
+Setup::setMode("live");
+Setup::setPublicKey("live_public_eGHyRPJlnkWZpCRWoeaqorkgTcM");
+//Configuration des informations de votre service/entreprise
+//Setup::setMasterKey("sO4bJTB1-VGno-uwPH-HRMT-6C5H4kNFu8Qn");
+//Setup::setPublicKey("test_public_tzTd3klc9WYzradGceuoHmaPvKR");
+//Setup::setPrivateKey("test_private_NHEmeiZn3nUAyq0sYprqRhJlYxH");
+//Setup::setToken("FWqNtl4ydmUdpsCcSlKa");
+//Setup::setMode("test"); // Optionnel. Utilisez cette option pour les paiements tests.
+Store::setLogoUrl("https://nasrulex.com/assets/img/Nasurlex-logo.png");
+Store::setName("NASRULEX"); // Seul le nom est requis
+Store::setTagline("Plateforme de Documentation Juridique");
+Store::setPhoneNumber("+221 77 377 77 66");
+Store::setWebsiteUrl("http://nasrulex.com");
         $em=$this->getDoctrine()->getManager();
         $co = new CheckoutInvoice();
         $pack=$repac->find($idpack);
